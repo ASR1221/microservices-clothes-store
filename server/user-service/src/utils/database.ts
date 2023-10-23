bb1901630e609500106a18fc764f55b978dbfcd8
asr <asr12211@outlook.com>
2023-10-23 20:49:52 +0300

    EDIT: modules import in database.ts in items & user services

--- server/user-service/src/utils/database.ts
+++ server/user-service/src/utils/database.ts
@@ -1,7 +1,8 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
 if (process.env.NODE_ENV !== "production") {
-   require("dotenv").config();
+   dotenv.config();
 }
 
 // Database connection
