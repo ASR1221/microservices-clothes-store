3030277373e75bed6f1ce33e9f6d2052675e7b3a
asr <asr12211@outlook.com>
2023-10-24 13:54:16 +0300

    FIX: authMiddleware fixed in cart

--- server/cart-service/src/middlewares/authMiddleware.ts
+++ server/cart-service/src/middlewares/authMiddleware.ts
@@ -9,8 +9,19 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 export default async function authMiddleware(req: Request, res: Response, next: NextFunction) {
+
+   if (!req.headers.authorization) {
+      const error = new Error("No Authorization header") as CustomError;
+      error.status = 401;
+      return next(error);
+   }
+
    try {
-      const authResponse = await fetch(`${process.env.USER_SERVICE_URL}/auth/check`);
+      const authResponse = await fetch(`${process.env.USER_SERVICE_URL}/auth/check`, {
+         headers: {
+            "Authorization": req.headers.authorization,
+         }
+      });
 
       if (!authResponse.ok) {
          const error = new Error("users-service response error. Check users-service and repeat this request.") as CustomError;
