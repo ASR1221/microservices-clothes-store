ec42082ae62e80e0ffdaf0dfa5dd0cd71e28cff6
asr <asr12211@outlook.com>
2024-01-20 17:28:31 +0300

    FIX: async await in checkAndRecreateSessionMiddleware

--- server/user-service/src/middlewares/checkAndRecreateSessionMiddleware.ts
+++ server/user-service/src/middlewares/checkAndRecreateSessionMiddleware.ts
@@ -1,8 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import checkAndRecreateSession from "../utils/checkAndRecreateSession";
 
-export default function checkAndRecreateSessionMiddleware(req: Request, res: Response, next: NextFunction) {
-   checkAndRecreateSession(req, next);
+export default async function checkAndRecreateSessionMiddleware(req: Request, res: Response, next: NextFunction) {
+   await checkAndRecreateSession(req, next);
 
    return next();
 }
