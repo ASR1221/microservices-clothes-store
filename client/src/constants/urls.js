e2c1530f1b1854b585b98ff393a6b1acfbfdfd6b
asr <asr12211@outlook.com>
2024-01-22 14:43:18 +0300

    ADD: urls of services

---
+++ client/src/constants/urls.js
@@ -0,0 +1,6 @@
+export const USER_SERVICE_URL = "http://localhost:3001";
+export const ITEM_SERVICE_URL = "http://localhost:3002";
+export const CART_SERVICE_URL = "http://localhost:3003";
+export const ORDER_SERVICE_URL = "http://localhost:3004";
+export const ADMIN_SERVICE_URL = "http://localhost:3005";
+export const CLIENT_SERVICE_URL = "http://localhost:3006";
