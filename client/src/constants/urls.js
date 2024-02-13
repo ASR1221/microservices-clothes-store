a9995b898c74396c381cda5956823ff877c501b0
asr <asr12211@outlook.com>
2024-02-14 00:13:20 +0300

    FIX: URLs in client are now dynamic according to env vars

--- client/src/constants/urls.js
+++ client/src/constants/urls.js
@@ -1,5 +1,8 @@
-export const USER_SERVICE_URL = "http://ms-asr-store-user-deployment";
-export const ITEM_SERVICE_URL = "http://ms-asr-store-item-deployment";
-export const CART_SERVICE_URL = "http://ms-asr-store-cart-deployment";
-export const ORDER_SERVICE_URL = "http://ms-asr-store-order-deployment";
-export const ADMIN_SERVICE_URL = "http://ms-asr-store-admin-deployment";
+// Why define it here?
+// The code did not use env vars and used static urls. So for simplicity I used it here again so the code will not change.
+
+export const USER_SERVICE_URL = import.meta.env.VITE_USER_SERVICE_URL;
+export const ITEM_SERVICE_URL = import.meta.env.VITE_ITEM_SERVICE_URL;
+export const CART_SERVICE_URL = import.meta.env.VITE_CART_SERVICE_URL;
+export const ORDER_SERVICE_URL = import.meta.env.VITE_ORDER_SERVICE_URL;
+export const ADMIN_SERVICE_URL = import.meta.env.VITE_ADMIN_SERVICE_URL;
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+const env = loadEnv(
+  'all',
+  "",
+);
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api/user': "http://ms-asr-store-user-deployment",
-      '/api/items': "http://ms-asr-store-item-deployment",
-      '/api/cart': "http://ms-asr-store-cart-deployment",
-      '/api/order': "http://ms-asr-store-order-deployment",
-      '/api/admin': "http://ms-asr-store-admin-deployment",
-      '/images': "http://ms-asr-store-item-deployment",
+      '/api/user': env.VITE_USER_SERVICE_URL,
+      '/api/items': env.VITE_ITEM_SERVICE_URL,
+      '/api/cart': env.VITE_CART_SERVICE_URL,
+      '/api/order': env.VITE_ORDER_SERVICE_URL,
+      '/api/admin': env.VITE_ADMIN_SERVICE_URL,
+      '/images': env.VITE_ITEM_SERVICE_URL,
     },
   },
 });
